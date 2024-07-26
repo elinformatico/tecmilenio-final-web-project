241f4b3be4b3d0b96ac09987fcac0632408cdd9d
elinformatico <donoe1985@gmail.com>
2024-07-25 18:16:20 -0600

    Modifying the Products name ans description

---
+++ project/js/products.js
@@ -0,0 +1,14 @@
+const products = [
+    { id: 1, name: 'Escritorio Moderno', description: 'Un Escritorio moderno para trabajar desde Home Office', price: '$2,500 MXN', image: 'images/product_1.jpg' },
+    { id: 2, name: 'Mueble para TV grande', description: 'Mueble para TV grande donde podras poner una TV de hasta 75 Pulgadas', price: '$3,000 MXN', image: 'images/product_2.jpg' },
+    { id: 3, name: 'Mesa para Sala Moderno', description: 'Mesa con 6 Sillas modernas, color blanco', price: '$8,500 MXN', image: 'images/product_3.jpg' },
+    { id: 4, name: 'Product 4', description: 'This is a short description of product 4.', price: '$40.00', image: 'images/product_4.jpg' },
+    { id: 5, name: 'Product 5', description: 'This is a short description of product 5.', price: '$50.00', image: 'images/product_5.jpg' },
+    { id: 6, name: 'Product 6', description: 'This is a short description of product 5.', price: '$50.00', image: 'images/product_6.jpg' },
+    { id: 7, name: 'Product 7', description: 'This is a short description of product 5.', price: '$50.00', image: 'images/product_7.jpg' },
+    { id: 8, name: 'Product 8', description: 'This is a short description of product 5.', price: '$50.00', image: 'images/product_8.jpg' },
+    { id: 9, name: 'Product 9', description: 'This is a short description of product 5.', price: '$50.00', image: 'images/product_9.jpg' },
+    { id: 10, name: 'Product 10', description: 'This is a short description of product 5.', price: '$50.00', image: 'images/product_10.jpg' },
+    { id: 11, name: 'Product 11',description: 'This is a short description of product 5.', price: '$50.00', image: 'images/product_11.jpg' },
+    { id: 12, name: 'Product 12', description: 'This is a short description of product 5.', price: '$50.00', image: 'images/product_12.jpg' },
+  ];
---
+++ project/js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+  
+    const productsContainer = document.getElementById('products-container');
+  
+    products.forEach(product => {
+      const productCard = document.createElement('div');
+      productCard.classList.add('col-lg-4', 'col-md-6', 'col-sm-12', 'product-card');
+  
+      productCard.innerHTML = `
+      <div class="card">
+        <img src="${product.image}" 
+               class="card-img-top product-thumbnail" 
+               alt="${product.name}" 
+               data-toggle="modal" 
+               data-target="#productModal" 
+               data-image="${product.image}">
+        <div class="card-body">
+          <h5 class="card-title">${product.name}</h5>
+          <p class="card-text">${product.description}</p>
+          <p class="card-text">${product.price}</p>
+          <button class="btn btn-primary">Agregar al Carrito</button>
+        </div>
+      </div>`;
+  
+      productsContainer.appendChild(productCard);
+    });
+    // Event listener for product thumbnails
+    document.querySelectorAll('.product-thumbnail').forEach(thumbnail => {
+      thumbnail.addEventListener('click', function() {
+        const imageSrc = this.getAttribute('data-image');
+        document.getElementById('modalProductImage').src = imageSrc;
+      });
+    });
+  });
+  
